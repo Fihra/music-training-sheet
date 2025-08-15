1e28da432cb822d2953a239b318a04b71f15e697
Fabian <fabrofabian@gmail.com>
2025-08-14 21:33:59 -0700

    added a navbar

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,12 @@
+const Navbar = () => {
+    return (
+        <ul>
+            <li>Home</li>
+            <li>Generate</li>
+            <li>Login/Sign up</li>
+
+        </ul>
+    )
+}
+
+export default Navbar;
