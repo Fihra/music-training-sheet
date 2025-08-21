98adda065f7584354669990b029d6550516aab18
Fabian <fabrofabian@gmail.com>
2025-08-20 20:02:16 -0700

    removed pitchtest from home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
 import styles from "./page.module.css";
 import About from "./components/About";
-import PitchTest from "./components/PitchTest";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <h1>Music Training</h1>
       <About/>
-      <PitchTest/>
     </div>
   );
 }
