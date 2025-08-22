cbcfe56ce6d210af7cafa372d70a8a65d35470cc
Fabian <fabrofabian@gmail.com>
2025-08-22 12:39:27 -0700

    created post request for music sheets

---
+++ src/app/api/musicsheets/route.ts
@@ -0,0 +1,52 @@
+import mysql from "mysql2/promise";
+import { NextResponse, NextRequest } from "next/server";
+import { db } from "@/lib/db";
+import { ResultSetHeader, RowDataPacket } from "mysql2/promise";
+
+interface User {
+    user_id: number;
+    email: string;
+    musicSheets: string[]
+}
+
+export async function POST(req: NextRequest, res: NextResponse) {
+    try {
+        const body = await req.json();
+        const { musicSheet, user_id} = body;
+
+        const connection = await db.getConnection();
+
+        const [result] = await connection.query<ResultSetHeader>(
+            "INSERT INTO musicsheets (user_id, sheet_tones) VALUES (?, ?)", [
+                user_id, JSON.stringify(musicSheet)
+            ]
+        )
+
+        const newMusicSheetID = result.insertId;
+
+        const [users] = await connection.query<RowDataPacket[]>("SELECT * FROM users WHERE user_id = ?", [user_id]);
+
+        if(users.length === 0) {
+            return NextResponse.json({ error: 'invalid user'}, {status: 400})
+        }
+
+        const user = users[0] as User;
+
+        const currentMusicSheets = Array.isArray(user.musicSheets) ? user.musicSheets : [];
+        const updatedMusicSheets = [...currentMusicSheets, newMusicSheetID];
+
+        await connection.query("UPDATE users SET musicSheets = ? WHERE user_id = ?",  [
+            JSON.stringify(updatedMusicSheets),
+            user_id
+        ])
+        await connection.commit();
+
+        return Response.json({ message: "Music Sheet Created"});
+
+    }catch(error:any) {
+        console.error(error);
+        return Response.json({
+            message: "Error: " + error.message
+        }, {status: 500});
+    }
+}
