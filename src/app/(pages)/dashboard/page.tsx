f9a130143aca7e900af4b0440639f38337865379
Fabian <fabrofabian@gmail.com>
2025-08-22 14:57:49 -0700

    get request for music sheets for dashboard

--- src/app/(pages)/dashboard/page.tsx
+++ src/app/(pages)/dashboard/page.tsx
@@ -1,6 +1,8 @@
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import { redirect } from "next/navigation";
+import { db } from "@/lib/db";
+import NoteSequence from "@/app/components/NoteSequence";
 
 export default async function dashboard() {  
     const session = await getServerSession(authOptions);
@@ -8,11 +10,24 @@ export default async function dashboard() {
     if(!session){
         redirect("/login");
     }
+
+    const [rows] = await db.query(
+        "SELECT * FROM musicSheets WHERE user_id = ?",
+        [session.user.user_id]
+    );
+
+    const showSequences = () => {
+        return rows.map((musicSheet, key) => {
+            return <NoteSequence key={key} sheet_tones={musicSheet.sheet_tones}/>
+        })
+    }
     
     return(
         <div>
             My dashboard logged in.
             <h1>Welcome {session.user?.email}</h1>
+            {/* <p>{JSON.stringify(rows, null, 2)}</p> */}
+            {showSequences()}
         </div>
     )
 }
