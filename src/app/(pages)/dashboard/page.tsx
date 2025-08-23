1849a901b2989c47940bc372671fff72e5df212c
Fabian <fabrofabian@gmail.com>
2025-08-22 18:14:27 -0700

    switched server component abd pass musicSheetID

--- src/app/(pages)/dashboard/page.tsx
+++ src/app/(pages)/dashboard/page.tsx
@@ -1,8 +1,11 @@
+'use server';
+
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import { redirect } from "next/navigation";
 import { db } from "@/lib/db";
 import NoteSequence from "@/app/components/NoteSequence";
+import styles from "../../page.module.css";
 
 export default async function dashboard() {  
     const session = await getServerSession(authOptions);
@@ -17,15 +20,15 @@ export default async function dashboard() {
     );
 
     const showSequences = () => {
-        return rows.map((musicSheet, key) => {
-            return <NoteSequence key={key} sheet_tones={musicSheet.sheet_tones}/>
+        return rows.map((musicSheet) => {
+            return <NoteSequence key={musicSheet.music_sheet_id} sheet_tones={musicSheet.sheet_tones} musicSheetID={musicSheet.music_sheet_id}/>
         })
     }
     
     return(
-        <div>
-            My dashboard logged in.
-            <h1>Welcome {session.user?.email}</h1>
+        <div className={styles.dashboardContainer}>
+            <h1>Welcome back!</h1>
+            <p>Email: {session.user?.email}</p>
             {/* <p>{JSON.stringify(rows, null, 2)}</p> */}
             {showSequences()}
         </div>
