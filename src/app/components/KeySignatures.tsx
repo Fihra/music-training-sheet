96a2fa3e049ea634ae1ffcbdfcb0cf62df3923ca
Fabian <fabrofabian@gmail.com>
2025-09-02 22:38:30 -0700

    created key signatures file to add scales

--- src/app/components/KeySignatures.tsx
+++ src/app/components/KeySignatures.tsx
@@ -1,4 +1,67 @@
 export const keySignatures = [
-"C","F", "Bb", "Eb", "Ab", "Db","Gb","B","E","A","D","G","Am","Dm","Gm","Cm","Fm","Bbm","Ebm","G#m", "C#m","F#m","Bm","Em"
+"C","F", "Bb", "Eb", "Ab", "Db","Gb","B","E","A","D","G", "C#", "F#", "Am","Dm","Gm","Cm","Fm","Bbm","Ebm","D#m", "G#m", "C#m","F#m","Bm","Em"
 ]
+
+export const allPitches = ["C", "C#", "Db", "D", "D#", "Eb", "E", "E#", "F", "F#", "Gb", "G", "G#", "Ab", "A", "A#", "Bb", "B", "B#"];
+
+export const scales: Record<string,string[]> = {
+    "C": ["C", "D", "E", "F", "G", "A", "B"],
+    "G": ["G", "A", "B", "C", "D", "E", "F#"],
+    "D": ["D", "E", "F#", "G", "A", "B", "C#"],
+    "A": ["A", "B", "C#", "D", "E", "F#", "G#"],
+    "E": ["E", "F#", "G#", "A", "B", "C#", "D#"],
+    "B": ["B", "C#", "D#", "E", "F#", "G#", "A#"],
+    "F#": ["F#", "G#", "A#", "B", "C#", "D#", "E#"],
+    "Gb": ["Gb", "Ab", "Bb", "Cb", "Db", "Eb", "F"],
+    "C#": ["C#", "D#", "E#", "F#", "G#", "A#", "B#"],
+    "F": ["F", "G", "A", "Bb", "C", "D", "E"],
+    "Bb": ["Bb", "C", "D", "Eb", "F", "G", "A"],
+    "Eb": ["Eb", "F", "G", "Ab", "Bb", "C", "D"],
+    "Ab": ["Ab", "Bb", "C", "Db", "Eb", "F", "G"],
+    "Db": ["Db", "Eb", "F", "Gb", "Ab", "Bb", "C"],
+    "Am": ["A", "B", "C", "D", "E", "F", "G"],
+    "Dm": ["D", "E", "F", "G", "A", "Bb", "C"],
+    "Gm": ["G", "A", "Bb", "C", "D", "Eb", "F"],
+    "Cm": ["C", "D", "Eb", "F", "G", "Ab", "Bb"],
+    "Fm": ["F", "G", "Ab", "Bb", "C", "Db", "Eb"],
+    "Bbm": ["Bb", "C", "Db", "Eb", "F", "Gb", "Ab"],
+    "Ebm": ["Eb", "F", "Gb", "Ab", "Bb", "Cb", "Db"],
+    "D#m": ["D#", "E#", "F#", "G#", "A#", "B", "C#"],
+    "G#m": ["G#", "A#", "B", "C#", "D#", "E", "F#"],
+    "C#m": ["C#", "D", "E", "F#", "G#", "A", "B"],
+    "F#m": ["F#", "G#", "A", "B", "C#", "D", "E"],
+    "Bm": ["B", "C#", "D", "E", "F#", "G", "A"],
+    "Em":["E", "F#", "G", "A", "B", "C", "D"]
+}
  
+export const aMinor = ["A", "B", "C", "D", "E", "F", "G"];
+export const dMinor = ["D", "E", "F", "G", "A", "Bb", "C"];
+export const gMinor = ["G", "A", "Bb", "C", "D", "Eb", "F"];
+export const cMinor = ["C", "D", "Eb", "F", "G", "Ab", "Bb"];
+export const fMinor = ["F", "G", "Ab", "Bb", "C", "Db", "Eb"];
+export const BbMinor = ["Bb", "C", "Db", "Eb", "F", "Gb", "Ab"];
+export const EbMinor = ["Eb", "F", "Gb", "Ab", "Bb", "Cb", "Db"];
+export const dSharpMinor = ["D#", "E#", "F#", "G#", "A#", "B", "C#", "D#"];
+export const gSharpMinor = ["G#", "A#", "B", "C#", "D#", "E", "F#"];
+export const cSharpMinor = ["C#", "D", "E", "F#", "G#", "A", "B"];
+export const fSharpMinor = ["F#", "G#", "A", "B", "C#", "D", "E"]
+export const bMinor = ["B", "C#", "D", "E", "F#", "G", "A"];
+export const eMinor = ["E", "F#", "G", "A", "B", "C", "D"];
+
+export const cMajor = ["C", "D", "E", "F", "G", "A", "B"];
+export const gMajor = ["G", "A", "B", "C", "D", "E", "F#"];
+export const dMajor = ["D", "E", "F#", "G", "A", "B", "C#"];
+export const aMajor = ["A", "B", "C#", "D", "E", "F#", "G#"];
+export const eMajor = ["E", "F#", "G#", "A", "B", "C#", "D#"];
+export const bMajor = ["B", "C#", "D#", "E", "F#", "G#", "A#"];
+export const fSharpMajor = ["F#", "G#", "A#", "B", "C#", "D#", "E#"];
+export const GbMajor = ["Gb", "Ab", "Bb", "Cb", "Db", "Eb", "F"];
+export const cSharpMajor = ["C#", "D#", "E#", "F#", "G#", "A#", "B#"];
+export const gSharpMajor = ["G#", "A#",  "B#", "C#", "D#", "E#", "Fx"];
+export const fMajor = ["F", "G", "A", "Bb", "C", "D", "E"];
+export const BbMajor = ["Bb", "C", "D", "Eb", "F", "G", "A"];
+export const EbMajor = ["Eb", "F", "G", "Ab", "Bb", "C", "D"];
+export const AbMajor = ["Ab", "Bb", "C", "Db", "Eb", "F", "G"];
+export const DbMajor = ["Db", "Eb", "F", "Gb", "Ab", "Bb", "C"];
+
+
