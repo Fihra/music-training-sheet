9d75bea46a023ae02ed72c3d2a4bdfcc4e0615c6
Fabian <fabrofabian@gmail.com>
2025-09-02 18:22:30 -0700

    added Inconsolata font family

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,14 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Inconsolata } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
 import { Providers } from "./components/Providers";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const inconsolata = Inconsolata({
+  subsets: ['latin'],
+  weight: ['200', '300', '400', '500', '600', '700'],
+  variable: '--font-inconsolata'
+})
 
 export const metadata: Metadata = {
   title: "Music Training",
@@ -26,7 +22,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={`${inconsolata.variable}`}>
         <Providers>
         <Navbar/>
         {children}
